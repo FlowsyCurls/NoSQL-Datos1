59be5b492ad153dfebbf697250381bdb18341661
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2019-05-07 00:23:11 -0600

    Create pruebas.java

---
+++ src/Arboles/pruebas.java
@@ -0,0 +1,33 @@
+package Arboles;
+
+import java.util.ArrayList;
+
+public class pruebas {
+	public static void main(String[]args) {
+		persona aldo1=new persona(1,"aldo");
+		persona aldo2=new persona(2,"aldo1");
+		persona aldo3=new persona(3,"aldo2");
+		
+		AVL<Integer> ejemplo= new AVL<Integer>();
+		//         el "1" es la llave
+		ejemplo.insert(1,aldo1);
+		ejemplo.insert(2,aldo2);
+		ejemplo.insert(3,aldo3);
+		
+		System.out.print(ejemplo.buscarDato(3)+"\n");
+		System.out.print(ejemplo.buscarDato(2)+"\n");
+		System.out.print(ejemplo.buscarDato(3)+"\n");
+		
+		System.out.print("Original:"+aldo1+"\n");
+		System.out.print("Original:"+aldo2+"\n");
+		System.out.print("Original:"+aldo3+"\n"+"\n");
+		
+		//persona a = new persona(22,"pepe");
+		//ejemplo.buscarDato(1)=a;
+		//System.out.print(a.nombre);
+		//System.out.print(ejemplo.buscarDato(1)+"+n");
+		//System.out.print(ejemplo.buscarDato(2)+"\n");
+		//System.out.print(ejemplo.buscarDato(1)+"\n");
+		
+	}
+}
