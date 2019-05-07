480620565c917154409b67b3b4316e08bb21af88
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2019-05-07 00:22:23 -0600

    Create persona.java

---
+++ src/Arboles/persona.java
@@ -0,0 +1,25 @@
+package Arboles;
+
+public class persona {
+	public int edad;
+	public String nombre;
+	public persona(int edad,String nombre) {
+		this.edad=edad;
+		this.nombre=nombre;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
